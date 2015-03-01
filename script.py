155dc38a4e441f5d923da6962933d0170a7192f1
Xylias <soerenklein98@gmail.com>
2015-03-01 12:51:24 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,12 +5,22 @@ import time
 
 pi = pigpio.pi()
 
+def color(red, green, blue):
+  pi.set_PWM_dutycycle(27, red)
+  pi.set_PWM_dutycycle(22, green)
+  pi.set_PWM_dutycycle(17, blue)
+  return true
+
 n = 255
 blue = 0
 for i in range(1,n+1):
   blue = blue + 1
   pi.set_PWM_dutycycle(17, blue)
   time.sleep(0.01)
+
+color(255, 255, 255)
+time.sleep(0.1)
+color(0, 0, 0)
 
 blue = 255
 for i in range(1,n+1):
@@ -21,9 +31,3 @@ for i in range(1,n+1):
 color(255, 255, 255)
 time.sleep(0.1)
 color(0, 0, 0)
-
-def color(red, green, blue):
-  pi.set_PWM_dutycycle(27, red)
-  pi.set_PWM_dutycycle(22, green)
-  pi.set_PWM_dutycycle(17, blue)
-  return true
