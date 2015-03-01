96abba0d5595012f858da7dfdfb443764769a6ce
Xylias <soerenklein98@gmail.com>
2015-03-01 14:47:53 +0100

    Update programm.py

--- programm.py
+++ programm.py
@@ -5,33 +5,52 @@ import time
 
 pi = pigpio.pi()
 
-red = 0
-green = 0
-blue = 0
 
-def color(var1, var2, var3):
-  red = var1
-  green = var2
-  blue = var3
-  print red
+class LED(object):
+  def __init__(pinR, pinG, pinB):
+    self.__red = 0
+    self.__green = 0
+    self.__blue = 0
+    self.__pinR = pinR
+    self.__pinG = pinG
+    self.__pinB = pinB
+  
+  def color(var1, var2, var3):
+    self.__red = var1
+    self.__green = var2
+    self.__blue = var3
+    return True
+  
+  def update():
+    pi.set_PWM_dutycycle(self.__pinR, self.__red)
+    pi.set_PWM_dutycycle(self.__pinG, self.__green)
+    pi.set_PWM_dutycycle(self.__pinB, self.__blue)
+    return True
+  
+  def getRed():
+    return self.__red
+  
+  def getGreen():
+    return self.__green
+  
+  def getBlue():
+    return self.__Blue
+  
+  def getPinR():
+    return self.__pinR
+  
+  def getPinG():
+    return self.__pinG
+  
+  def getPinB():
+    return self.__pinB
+  
+  def setPins(var1, var2, var3):
+    self.__pinR = var1
+    self.__pinG = var2
+    self.__pinB = var3
+    return True
 
-def update():
-  pi.set_PWM_dutycycle(27, red)
-  pi.set_PWM_dutycycle(22, green)
-  pi.set_PWM_dutycycle(17, blue)
-  print red
-
-color(0, 0, 0)
-pi.set_PWM_dutycycle(22, 255)
-pi.set_PWM_dutycycle(22, 0)
-time.sleep(0.1)
-
-var = 1
-while var == 1 :
-  color(255, 255, 255)
-  print red
-  update()
-  time.sleep(0.1)
-  color(0, 0, 0)
-  update()
-  time.sleep(0.1)
+led = LED(27, 22, 17)
+led.color(255, 255, 255)
+led.update()
