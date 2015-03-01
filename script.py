2a31f385545d2b16e00ae4e8fa38b8091877844e
Xylias <soerenklein98@gmail.com>
2015-03-01 12:49:18 +0100

    Update script.py

--- script.py
+++ script.py
@@ -11,3 +11,19 @@ for i in range(1,n+1):
   blue = blue + 1
   pi.set_PWM_dutycycle(17, blue)
   time.sleep(0.01)
+
+blue = 255
+for i in range(1,n+1):
+  blue = blue - 1
+  pi.set_PWM_dutycycle(17, blue)
+  time.sleep(0.01)
+
+color(255, 255, 255)
+time.sleep(0.1)
+color(0, 0, 0)
+
+def color(red, green, blue):
+  pi.set_PWM_dutycycle(27, red)
+  pi.set_PWM_dutycycle(22, green)
+  pi.set_PWM_dutycycle(17, blue)
+  return true
