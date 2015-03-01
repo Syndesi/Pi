a74b343008f080343faaef81f072a4fa6e783c44
Xylias <soerenklein98@gmail.com>
2015-03-01 16:52:25 +0100

    Update color.py

--- color.py
+++ color.py
@@ -1,37 +1,32 @@
-# This script need pigpio for operating.
+class Color(object):
+  def __init__(self, var1, var2, var3):
+    self.__red = var1
+    self.__green = var2
+    self.__blue = var3
+  
+  def setColor(self, var1, var2, var3):
+    self.__red = var1
+    self.__green = var2
+    self.__blue = var3
+    return True
 
-import pigpio
-import time
-
-pi = pigpio.pi()
-
-def color(red, green, blue):
-  pi.set_PWM_dutycycle(27, red)
-  pi.set_PWM_dutycycle(22, green)
-  pi.set_PWM_dutycycle(17, blue)
-
-
-color(0, 0, 0)
-time.sleep(0.1)
-color(0, 0, 2)
-time.sleep(0.1)
-color(0, 0, 4)
-time.sleep(0.1)
-color(0, 0, 8)
-time.sleep(0.1)
-color(0, 0, 16)
-time.sleep(0.1)
-color(0, 0, 32)
-time.sleep(0.1)
-color(0, 0, 64)
-time.sleep(0.1)
-color(0, 0, 128)
-time.sleep(0.1)
-color(0, 0, 255)
-time.sleep(0.1)
-color(0, 0, 0)
-time.sleep(0.1)
-color(255, 255, 255)
-time.sleep(0.1)
-color(0, 0, 0)
-time.sleep(0.1)
+  def getRed(self):
+    return self.__red
+  
+  def getGreen(self):
+    return self.__green
+  
+  def getBlue(self):
+    return self.__blue
+  
+  def setRed(self, var1):
+    self.__red = var1
+    return True
+  
+  def setGreen(self, var1):
+    self.__green = var1
+    return True
+  
+  def setBlue(self, var1):
+    self.__blue = var1
+    return True
