8422e3d9555561e53d1c730f27c4360c40dcebdb
Xylias <soerenklein98@gmail.com>
2015-03-01 13:01:50 +0100

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 
 import pigpio
 import time
+import math
 
 pi = pigpio.pi()
 
@@ -12,26 +13,22 @@ def color(red, green, blue):
 
 n = 255
 blue = 0
-for i in range(1,n+1):
+color(0, 0, 0)
+
+while (math.sqrt(blue) <= 255):
   blue = blue + 1
-  pi.set_PWM_dutycycle(17, blue)
-  time.sleep(0.01)
+  color(math.sqrt(blue), 0, 0)
 
-color(255, 255, 255)
-time.sleep(0.1)
-color(0, 0, 0)
+#for i in range(1,n+1):
+#  blue = blue + 1
+#  pi.set_PWM_dutycycle(17, blue)
+#  time.sleep(0.01)
 
-blue = 255
-for i in range(1,n+1):
-  blue = blue - 1
-  pi.set_PWM_dutycycle(17, blue)
-  time.sleep(0.01)
-
-color(255, 255, 255)
-time.sleep(0.1)
-color(0, 0, 0)
-
-
+#blue = 255
+#for i in range(1,n+1):
+#  blue = blue - 1
+#  pi.set_PWM_dutycycle(17, blue)
+#  time.sleep(0.01)
 
 color(255, 0, 0)
 time.sleep(0.1)
@@ -45,4 +42,4 @@ color(0, 0, 255)
 time.sleep(0.1)
 color(255, 0, 255)
 time.sleep(0.1)
-color(0, 0, ,0)
+color(0, 0, 0)
