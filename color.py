a6671e77e40ba64c3312c71c50e25a641282eed5
Xylias <soerenklein98@gmail.com>
2015-03-01 17:13:05 +0100

    Update color.py

--- color.py
+++ color.py
@@ -30,3 +30,13 @@ class Color(object):
   def setBlue(self, var1):
     self.__blue = var1
     return True
+
+white = Color(255, 255, 255)
+red = Color(255, 0, 0)
+blue = Color(0, 0, 255)
+yellow = Color(255, 255, 0)
+green = Color(0, 255, 0)
+black = Color(0, 0, 0)  # black is transparent, because the LED is off
+purple = Color(255, 0, 255)
+cyan = Color(0, 255, 255)
+orange = Color(128, 255, 0)
