3a8447779d0dcf3dac59b96870c8243ba081e320
Xylias <soerenklein98@gmail.com>
2015-03-01 12:19:15 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,21 @@
+# This script need pigpio for operating.
+
+import pigpio
+
+pi.set_PWM_dutycycle(17, 255)
+
+sleep(1000)
+
+pi.set_PWM_dutycycle(17, 128)
+
+sleep(1000)
+
+pi.set_PWM_dutycycle(17, 64)
+
+sleep(1000)
+
+pi.set_PWM_dutycycle(17, 32)
+
+sleep(1000)
+
+pi.set_PWM_dutycycle(17, 16)
