fd7245f2a6a67c063b08ca3d437dfdcc53c639f8
Xylias <soerenklein98@gmail.com>
2015-03-01 14:17:21 +0100

    Update programm.py

--- programm.py
+++ programm.py
@@ -10,15 +10,15 @@ green = 0
 blue = 0
 
 def color(red2, green2, blue2):
-  red = red2
-  green = green2
-  blue = blue2
+  red2 = red
+  green2 = green
+  blue2 = blue
 
 def update():
   pi.set_PWM_dutycycle(27, red)
   pi.set_PWM_dutycycle(22, green)
   pi.set_PWM_dutycycle(17, blue)
-  print(red, ", ", green, ", ", blue)
+  print("R: ", red, " G: ", green, " B: ", blue)
 
 color(0, 0, 0)
 pi.set_PWM_dutycycle(22, 255)
